5020c6cb3a5a7bd0134f1479ff58bf782f0d7cbd
congyu711 <sxlxcsxlxc@gmail.com>
2023-03-30 17:19:45 +0800

    basic deadlock detect alg

--- deadlockDetector/deadlockDetector.cc
+++ deadlockDetector/deadlockDetector.cc
@@ -24,4 +24,34 @@ int main()
     for(auto &e:avil_resources) e=gen()%10;
     Kdtree::KdTree tree(&nodes);
     // No... This kdtree is not able to do range query. It can only handle knn queries.
+
+    //-----------------------------------
+    // the basic algorithm
+    vector<bool> fin(n);
+    auto __avil_resources=avil_resources;
+    int cnt=0;
+    while(1)
+    {
+        bool flag=0;
+        for(int i=0;i<n;i++)
+        {
+            if(fin[i])  continue;
+            bool violated=0;
+            for(int j=0;j<m;j++)    if(required_resources[i][j]>__avil_resources[j])  violated=1;
+            if(!violated)   // run the process. allocate the required resources
+            {
+                fin[i]=1;
+                cnt++;
+                flag=1;
+                for(int j=0;j<m;j++)    __avil_resources[j]+=allocated_resources[i][j];
+            }
+        }
+        if(!flag)
+        {
+            if(cnt!=n)  cout<<"deadlock!\n";
+            else    cout<<"no deadlock\n";
+            break;
+        }
+    }
+    //-----------------------------------
 }
